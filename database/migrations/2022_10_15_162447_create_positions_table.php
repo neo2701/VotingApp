a6a9988a34de6ca8a25a104e925102ed8e3eb0d9
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-10-16 11:11:22 +0000

    Update fields

--- database/migrations/2022_10_15_162447_create_positions_table.php
+++ database/migrations/2022_10_15_162447_create_positions_table.php
@@ -15,8 +15,7 @@ return new class extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique();
-            $table->string('description')->nullable();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
