92dc50fcac2cbe13d2f0dea7334aa27856b9787f
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-10-17 04:14:17 +0000

    Update Election.php

--- app/Models/Election.php
+++ app/Models/Election.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Election extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'starts_at',
+        'ends_at',
+    ];
+
+    protected $casts = [
+        'starts_at' => 'datetime',
+        'ends_at' => 'datetime',
+    ];
 }
