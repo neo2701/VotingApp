9ffd31aac6134e31150744289f0135a73d1d1067
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-10-17 04:14:39 +0000

    Update position colu;mn name string length to 50

--- database/migrations/2022_10_15_162447_create_positions_table.php
+++ database/migrations/2022_10_15_162447_create_positions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name', 50)->unique();
             $table->timestamps();
         });
     }
